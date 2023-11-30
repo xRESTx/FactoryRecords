cb5d096d79dd10c8aae8250ccf8d4d37fb55e950
REST <88982949+xRESTx@users.noreply.github.com>
2023-11-30 15:26:41 +0300

    Update Task function

--- First.cpp
+++ First.cpp
@@ -4,6 +4,7 @@
 #include <conio.h>
 #include <windows.h>
 #include <iomanip>
+#include <map>
 
 using namespace std;
 
@@ -32,6 +33,183 @@ MaterialRecord* createRecord( int id, int factoryNumber, int branchNumber, const
 	newRecord->next = nullptr;
 	return newRecord;
 }
+
+//#############################################Prototype functions####################################//
+
+void idCheck(MaterialRecord*& head, int& id);
+void addRecord(MaterialRecord*& head, MaterialRecord* newRecord);
+void displayRecords(MaterialRecord* head);
+void deleteRecord(MaterialRecord*& head, int idToDelete);
+void editRecord(MaterialRecord* head, int idToEdit);
+
+void saveToTextFile(MaterialRecord* head, const string& filename);
+void saveToBinaryFile(MaterialRecord* head, const char* filename);
+void loadFromTextFile(MaterialRecord*& head, const string& filename);
+void loadFromBinaryFile(MaterialRecord*& head, const string& filename)
+
+bool compareByColumn(const MaterialRecord& a, const MaterialRecord& b, int column);
+void swap(MaterialRecord* a, MaterialRecord* b);
+void sortRecords(MaterialRecord*& head, int column);
+
+void releaseMemory(MaterialRecord*& head);
+void exitProgram(MaterialRecord*& head);
+
+void searchByLastName(MaterialRecord* head, const string& lastName);
+
+void generateReport(MaterialRecord* head);
+void calculateEndPeriodValueByBranch(MaterialRecord* head);
+void calculateEndPeriodValueByFactory(MaterialRecord* head);
+
+//#############################################MAIN####################################//
+
+int main() {
+	MaterialRecord* records = nullptr; // голова списка записей
+
+	// defoltnoe(стартовое) заполнение таблицы
+	addRecord(records, createRecord(1, 1, 101, "Smith", 5000.0, 1000.0, 500.0));
+	addRecord(records, createRecord(2, 1, 101, "Johnson", 7000.0, 1500.0, 800.0));
+	addRecord(records, createRecord(3, 2, 201, "Williams", 6000.0, 1200.0, 700.0));
+	addRecord(records, createRecord(4, 2, 101, "Anderson", 400.0, 1500.0, 100.0));
+	addRecord(records, createRecord(5, 1, 102, "Besson", 70000.0, 1100.0, 0.0));
+
+	int c,i=0;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	string print_main[14]= {"Menu:","View Records","Add Record","Delete Record","Edit Record",
+	                        "Save Records to file.txt","Save Records to file.bin","Load Records from file.txt","Load Records from file.bin",
+	                        "Sort Records","Search by LastName","Clear Records","Generate Report","Exit"
+	                       };
+	int int_print_main = 14;
+	while(true) {
+		do {
+			for(int j=0; j<int_print_main; j++) {
+				if(j==i) {
+					SetConsoleTextAttribute(hConsole, 10);
+					cout<<" -> "<<print_main[j]<<endl;
+				} else
+					cout<<"    "<<print_main[j]<<endl;
+				SetConsoleTextAttribute(hConsole, 15);
+			}
+			c=getch();
+			if(c==115 || c==80) i++;
+			if(c==119 || c==72) i--;
+			if(i<0) i=0;
+			if(i>int_print_main-1) i=int_print_main-1;
+			system("cls");
+		} while(c!=13);
+
+
+		switch (i) {
+			case 1: {
+				cout << "\nMaterial Records:" << endl;
+				displayRecords(records);
+				break;
+			}
+			case 2: {
+				int id, factory, branch;
+				string lastName;
+				double start, received, disposed;
+				
+				cout << "Enter ID: ";
+				cin >> id;
+				idCheck(records, id);
+				cout << "Enter Factory Number: ";
+				cin >> factory;
+				cout << "Enter Branch Number: ";
+				cin >> branch;
+				cout << "Enter Last Name: ";
+				cin >> lastName;
+				cout << "Enter Start Value: ";
+				cin >> start;
+				cout << "Enter Received Value: ";
+				cin >> received;
+				cout << "Enter Disposed Value: ";
+				cin >> disposed;
+				
+				addRecord(records, createRecord(id, factory, branch, lastName, start, received, disposed));
+				break;
+			}
+			case 3: {
+				int idToDelete;
+				cout << "Enter ID of the record to delete: ";
+				cin >> idToDelete;
+				deleteRecord(records, idToDelete);
+				break;
+			}
+			case 4: {
+				int idToEdit;
+				cout << "Enter ID of the record to edit: ";
+				cin >> idToEdit;
+				editRecord(records, idToEdit);
+				break;
+			}
+			case 5: {
+				saveToTextFile(records, "material_records.txt");
+				break;
+			}
+			case 6: {
+				saveToBinaryFile(records, "material_records.bin");
+				break;
+			}
+			case 7: {
+				loadFromTextFile(records, "material_records.txt");
+				break;
+			}
+			case 8: {
+				loadFromBinaryFile(records, "material_records.bin");
+				break;
+			}
+			case 9: {
+				int columnSort;
+				cout << "(1)ID (2)Factory (3)Branch (4)Last Name (5)Start Value (6)Received Value (7)Disposed Value \n";
+				cout << "Enter number column for sort: ";
+				cin >> columnSort;
+				sortRecords(records, columnSort);
+				break;
+			}
+			case 10: {
+				string searchLastName;
+				cout << "Enter Last Name to search: ";
+				cin >> searchLastName;
+				searchByLastName(records, searchLastName);
+				break;
+			}
+			case 11: {
+				releaseMemory(records);
+				cout << "Records has been cleared.";
+				break;
+			}
+			case 12: {
+				generateReport(records);
+				break;
+			}
+			case 13: {
+				cout << "Maybe save Records?(Y=1/N=any symbol + Enter): ";
+				int saveChoice;
+				cin >> saveChoice;
+				if (saveChoice == 1) {
+					cout << "Format?(Bin=1/txt=any key + Enter): ";
+					int formatChoice;
+					cin >> formatChoice;
+					if (formatChoice == 1) {
+						saveToBinaryFile(records, "material_records.bin");
+					} else {
+						saveToTextFile(records, "material_records.txt");
+					}
+				}
+				exitProgram(records);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
 
 //##############################################Record handing###################################//
 
@@ -69,6 +247,7 @@ void displayRecords(MaterialRecord* head) {
 	cout << "----------------------------------------------------------------------------------------------" << endl;
 	MaterialRecord* current = head;
 	while (current != nullptr) {
+//		cout << "| " << setw(6) << current->id << " | " << setw(7) << current->factoryNumber << " | " << setw(6) << current->branchNumber << " | " << setw(13) << current->LastName << " | " << setw(14) << current->startValue << " | " << setw(16) << current->receivedValue << " | " << setw(16) << current->disposedValue << " |"<< endl;
 		printf("| %6d | %7d | %6d | %13s | %12.2f | %14.2f | %14.2f |\n", current->id, current->factoryNumber,
 		       current->branchNumber, current->LastName.c_str(), current->startValue, current->receivedValue,
 		       current->disposedValue);
@@ -391,7 +570,7 @@ void exitProgram(MaterialRecord*& head) {
 
 //##############################################poisk###################################//
 
-void searchByField(MaterialRecord* head, const string& lastName) {
+void searchByLastName(MaterialRecord* head, const string& lastName) {
 	MaterialRecord* current = head;
 	bool found = false;
 
@@ -404,7 +583,7 @@ void searchByField(MaterialRecord* head, const string& lastName) {
 				cout << "|   ID   | Factory | Branch |   Last Name   | Start Value | Received Value | Disposed Value |" << endl;
 				cout << "--------------------------------------------------------------------------------------------------------------" << endl;
 			}
-
+//			cout << "| " << setw(6) << current->id << " | " << setw(7) << current->factoryNumber << " | " << setw(6) << current->branchNumber << " | " << setw(13) << current->LastName << " | " << setw(14) << current->startValue << " | " << setw(16) << current->receivedValue << " | " << setw(16) << current->disposedValue << " |"<< endl;
 			printf("| %6d | %7d | %6d | %13s | %12.2f | %14.2f | %14.2f |\n", current->id, current->factoryNumber,
 			       current->branchNumber, current->LastName.c_str(), current->startValue,
 			       current->receivedValue, current->disposedValue);
@@ -419,174 +598,67 @@ void searchByField(MaterialRecord* head, const string& lastName) {
 	}
 }
 
-
-
-// Функция для подсчета и вывода ведомости
+//##############################################report###################################//
 void generateReport(MaterialRecord* head) {
-    double totalStartValue = 0.0, totalReceivedValue = 0.0, totalDisposedValue = 0.0;
-    int totalFactory = 0;
+    calculateEndPeriodValueByBranch(head);
+    calculateEndPeriodValueByFactory(head);
+}
 
-    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
-    cout << "| Factory | Starting Value | Received Value | Disposed Value | Ending Value |" << endl;
-    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+void calculateEndPeriodValueByBranch(MaterialRecord* head) {
+    map<pair<int, int>, double> endPeriodValueByBranch;
 
     MaterialRecord* current = head;
+
     while (current != nullptr) {
-        double endingValue = current->startValue + current->receivedValue - current->disposedValue;
-        totalStartValue += current->startValue;
-        totalReceivedValue += current->receivedValue;
-        totalDisposedValue += current->disposedValue;
+        pair<int, int> branchFactoryPair = make_pair(current->branchNumber, current->factoryNumber);
 
-        printf("| %7d | %14.2f | %14.2f | %14.2f | %12.2f |\n", current->factoryNumber, current->startValue,
-               current->receivedValue, current->disposedValue, endingValue);
-
-        totalFactory++;
+        if (endPeriodValueByBranch.find(branchFactoryPair) != endPeriodValueByBranch.end()) {
+            endPeriodValueByBranch[branchFactoryPair] += current->startValue + current->receivedValue - current->disposedValue;
+        } else {
+            endPeriodValueByBranch[branchFactoryPair] = current->startValue + current->receivedValue - current->disposedValue;
+        }
         current = current->next;
     }
 
-    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
-    cout << "|   Total   |" << setw(15) << totalStartValue << setw(16) << totalReceivedValue << setw(16)
-              << totalDisposedValue << setw(16) << (totalStartValue + totalReceivedValue - totalDisposedValue) << " |" << endl;
-    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
+    cout << "\nEnd Period Value by Branch:" << endl;
+    cout << "--------------------------------------" << endl;
+    cout << "| Factory | Branch | End Period Value |" << endl;
+    cout << "--------------------------------------" << endl;
+
+    cout << fixed << setprecision(2);
+
+    for (const auto& pairValue : endPeriodValueByBranch) {
+        cout << "| " << setw(7) << pairValue.first.second << " | " << setw(6) << pairValue.first.first << " | " << setw(16) << pairValue.second << " |" << endl;
+    }
+
+    cout << "--------------------------------------" << endl;
 }
 
+void calculateEndPeriodValueByFactory(MaterialRecord* head) {
+    map<int, double> endPeriodValueByFactory;
 
+    MaterialRecord* current = head;
+    
+    while (current != nullptr) {
+        // Если номер завода уже есть, добавляем стоимость к текущей сумме
+        if (endPeriodValueByFactory.find(current->factoryNumber) != endPeriodValueByFactory.end()) {
+            endPeriodValueByFactory[current->factoryNumber] += current->startValue + current->receivedValue - current->disposedValue;
+        } else { // Иначе добавляем новую запись
+            endPeriodValueByFactory[current->factoryNumber] = current->startValue + current->receivedValue - current->disposedValue;
+        }
+        current = current->next;
+    }
 
+    cout << "\nEnd Period Value by Branch:" << endl;
+    cout << "-----------------------------" << endl;
+    cout << "| Factory | End Period Value |" << endl;
+    cout << "-----------------------------" << endl;
 
-int main() {
-	MaterialRecord* records = nullptr; // голова списка записей
+    cout << fixed << setprecision(2);
 
-	// defoltnoe(стартовое) заполнение таблицы
-//	addRecord(records, createRecord(1, 1, 101, "Smith", 5000.0, 1000.0, 500.0));
-//	addRecord(records, createRecord(2, 1, 102, "Johnson", 7000.0, 1500.0, 800.0));
-//	addRecord(records, createRecord(3, 2, 201, "Williams", 6000.0, 1200.0, 700.0));
+    for (const auto& pairValue : endPeriodValueByFactory) {
+        cout << "| " << setw(7) << pairValue.first <<  " | " << setw(16) << pairValue.second << " |" << endl;
+    }
 
-	int c,i=0;
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	string print_main[14]= {"Menu:","View Records","Add Record","Delete Record","Edit Record",
-	                        "Save Records to file.txt","Save Records to file.bin","Load Records from file.txt","Load Records from file.bin",
-	                        "Sort Records","Search by LastName","Clear Records","Generate Report","Exit"
-	                       };
-	int int_print_main = 14;
-	while(true) {
-		do {
-			for(int j=0; j<int_print_main; j++) {
-				if(j==i) {
-					SetConsoleTextAttribute(hConsole, 10);
-					cout<<" -> "<<print_main[j]<<endl;
-				} else
-					cout<<"    "<<print_main[j]<<endl;
-				SetConsoleTextAttribute(hConsole, 15);
-			}
-			c=getch();
-			if(c==115 || c==80) i++;
-			if(c==119 || c==72) i--;
-			if(i<0) i=0;
-			if(i>int_print_main-1) i=int_print_main-1;
-			system("cls");
-		} while(c!=13);
-
-
-		switch (i) {
-			case 1: {
-				cout << "\nMaterial Records:" << endl;
-				displayRecords(records);
-				break;
-			}
-			case 2: {
-				int id, factory, branch;
-				string lastName;
-				double start, received, disposed;
-				
-				cout << "Enter ID: ";
-				cin >> id;
-				idCheck(records, id);
-				cout << "Enter Factory Number: ";
-				cin >> factory;
-				cout << "Enter Branch Number: ";
-				cin >> branch;
-				cout << "Enter Last Name: ";
-				cin >> lastName;
-				cout << "Enter Start Value: ";
-				cin >> start;
-				cout << "Enter Received Value: ";
-				cin >> received;
-				cout << "Enter Disposed Value: ";
-				cin >> disposed;
-				
-				addRecord(records, createRecord(id, factory, branch, lastName, start, received, disposed));
-				break;
-			}
-			case 3: {
-				int idToDelete;
-				cout << "Enter ID of the record to delete: ";
-				cin >> idToDelete;
-				deleteRecord(records, idToDelete);
-				break;
-			}
-			case 4: {
-				int idToEdit;
-				cout << "Enter ID of the record to edit: ";
-				cin >> idToEdit;
-				editRecord(records, idToEdit);
-				break;
-			}
-			case 5: {
-				saveToTextFile(records, "material_records.txt");
-				break;
-			}
-			case 6: {
-				saveToBinaryFile(records, "material_records.bin");
-				break;
-			}
-			case 7: {
-				loadFromTextFile(records, "material_records.txt");
-				break;
-			}
-			case 8: {
-				loadFromBinaryFile(records, "material_records.bin");
-				break;
-			}
-			case 9: {
-				int columnSort;
-				cout << "(1)ID (2)Factory (3)Branch (4)Last Name (5)Start Value (6)Received Value (7)Disposed Value \n";
-				cout << "Enter number column for sort: ";
-				cin >> columnSort;
-				sortRecords(records, columnSort);
-				break;
-			}
-			case 10: {
-				string searchLastName;
-				cout << "Enter Last Name to search: ";
-				cin >> searchLastName;
-				searchByField(records, searchLastName);
-				break;
-			}
-			case 11: {
-				releaseMemory(records);
-				cout << "Records has been cleared.";
-				break;
-			}
-			case 12: {
-				generateReport(records);
-				break;
-			}
-			case 13: {
-				cout << "Maybe save Records?(Y=1/N=any symbol + Enter): ";
-				int saveChoice;
-				cin >> saveChoice;
-				if (saveChoice == 1) {
-					cout << "Format?(Bin=1/txt=any key + Enter): ";
-					int formatChoice;
-					cin >> formatChoice;
-					if (formatChoice == 1) {
-						saveToBinaryFile(records, "material_records.bin");
-					} else {
-						saveToTextFile(records, "material_records.txt");
-					}
-				}
-				exitProgram(records);
-			}
-		}
-	}
+    cout << "-----------------------------" << endl;
 }
